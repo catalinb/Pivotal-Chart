83e00745ac365aaf1fc039733360b55a1c4b8f24
Cătălin Badea <catalin.badea392@gmail.com>
2012-09-30 20:09:55 +0300

    Add test request to pivotal server.

--- main.js
+++ main.js
@@ -1,24 +1,58 @@
+
 // TODO: use an external file for login credentials
 var token = "d295943c69609f13a8ab03d6f2e22a87";
 var project_id = "646863";
-var base_url = "http://www.pivotaltracker.com/services/v3/projects/"
+var base_url = "http://www.pivotaltracker.com/services/v3/projects/";
 
-$(function() {
-  // get token
-  update();
-});
+// Load the Visualization API and the piechart package.
+google.load('visualization', '1.0', {'packages':['corechart']}, update);
+google.setOnLoadCallback(update);
 
 function update() {
-  // get current iteration
+  // get current iteration:
   $.ajax({
-    url : base_url + project_id + "/iterations",
+    url : base_url + project_id + "/iterations/current",
     crossDomain : true,
     dataType : "xml",
     headers : {
       "X-TrackerToken" : token
     }}).success(function(data, textStatus, jqXHR) {
-      alert("success: " + data);
+      parseResponse(data);
+      debug(data);
     }).error(function() {
       alert("error");
     });
 }
+
+var globalData = {}
+function parseResponse(data) {
+  console.log(data);
+  globalData = data;
+
+  // get iteration interval
+  startDate = data.getElementsByTagName("start")[0];
+  endDate = data.getElementsByTagName("finish")[0];
+  console.log(startDate);
+  $(".iteration").append(startDate.textContent);
+  $(".iteration").append(endDate.textContent);
+}
+
+function debug(data) {
+      var response = xml_to_string(data);
+      response = response.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+      $("#debug").append(response);
+}
+
+function xml_to_string(xml_node) {
+  if (xml_node.xml)
+    return xml_node.xml;
+  else if (XMLSerializer) {
+    var xml_serializer = new XMLSerializer();
+    return xml_serializer.serializeToString(xml_node);
+  } else {
+    alert("ERROR: Extremely old browser");
+    return "";
+  }
+}
+
+
